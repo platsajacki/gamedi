4b20186b3ef8b2fe0c02f8b8e9abd4dcf24738c6
Вячеслав Менюхов <menyukhov@bk.ru>
2023-10-27 13:26:31 +0300

    Add docstring and annotaton.

--- gamedi/games/managers.py
+++ gamedi/games/managers.py
@@ -2,10 +2,17 @@ from django.db.models import QuerySet, Manager
 
 
 class GameQuerySet(QuerySet):
-    def related_tables(self) -> QuerySet:
+    def related_tables(self) -> 'GameQuerySet':
+        """
+        Отимизирует запрос, присоединяя таблицы.
+        """
         return self.select_related('genre')
 
-    def published(self) -> QuerySet:
+    def published(self) -> 'GameQuerySet':
+        """
+        Возвращает QuerySet, который фильтрует опубликованые игры
+        и принадлежат опубликованным жанрам.
+        """
         return self.filter(
             is_published=True,
             genre__is_published=True
@@ -13,7 +20,11 @@ class GameQuerySet(QuerySet):
 
 
 class GameManager(Manager):
-    def get_queryset(self) -> QuerySet:
+    """
+    Возвращает QuerySet для модели Game
+    с выбранными связанными таблицами и фильтрацией для опубликованных игр.
+    """
+    def get_queryset(self) -> GameQuerySet:
         return (
             GameQuerySet(self.model)
             .related_tables()
