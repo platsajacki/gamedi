b2f2f58aecdf8713c83ffc71ab5078be6fa41167
Вячеслав Менюхов <menyukhov@bk.ru>
2023-10-27 22:04:36 +0300

    Optimize UserQuerySet.

--- gamedi/users/managers.py
+++ gamedi/users/managers.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.models import UserManager
+from django.db.models import QuerySet
 
 
 class CustomUserManager(UserManager):
     """Кастомный менеджер модели пользователя."""
-    ...
+    def related_games(self) -> QuerySet:
+        """
+        Возвращает QuerySet,
+        предварительно загружающий связанные игры пользователя
+        вместе с файлами и жанром.
+        """
+        return (
+            self
+            .prefetch_related(
+                'games__users_files',
+                'games__admin_files',
+                'games__genre',
+            )
+        )
