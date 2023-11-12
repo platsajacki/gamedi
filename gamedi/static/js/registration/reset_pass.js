3d527e6a3db592eb7cc4582f7924c9456e7bc5e1
Вячеслав Менюхов <menyukhov@bk.ru>
2023-11-12 18:40:26 +0300

    Add styles reset_confirm.

--- gamedi/static/js/registration/reset_pass.js
+++ gamedi/static/js/registration/reset_pass.js
@@ -5,6 +5,8 @@ import {
 const formClass = 'form.main'
 const idEmail = 'id_email'
 const newTextContentEmail = 'Электронная почта:'
+const idNewPass2 = 'id_new_password2'
+const newTextPass2 = 'Повторите пароль:'
 
 
 /** Активирует все прослушки 'reset_pass'. */
@@ -14,4 +16,5 @@ async function wiretapping () {
 
 
 await changeTextContentByInput(idEmail, newTextContentEmail)
+await changeTextContentByInput(idNewPass2, newTextPass2)
 await wiretapping()
