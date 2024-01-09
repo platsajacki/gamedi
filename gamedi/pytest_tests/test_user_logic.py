2df275745b93a87f156f5db5254caaba4045829e
Вячеслав Менюхов <menyukhov@bk.ru>
2024-01-09 20:59:29 +0300

    Add TestUserLogic.

---
+++ gamedi/pytest_tests/test_user_logic.py
@@ -0,0 +1,55 @@
+import pytest
+from pytest_lazyfixture import lazy_fixture as lf
+
+from http import HTTPStatus
+
+from django.http import HttpResponse
+from django.test.client import Client
+from django.urls import reverse
+
+from users.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+class TestUserLogic:
+    def test_anonymous_can_registation(self, client: Client, registration_data: dict[str, str]):
+        """Анонимный пользователь может успешно зарегистрироваться."""
+        old_count_users: int = User.objects.count()
+        response: HttpResponse = client.post(reverse('registration'), data=registration_data)  # type: ignore
+
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == reverse('login')
+        assert old_count_users < User.objects.count()
+
+    def test_authorized_user_can_not_registation(self, user_client: Client, registration_data: dict[str, str]):
+        """Автороризованный пользователь не может успешно зарегистрироваться."""
+        old_count_users: int = User.objects.count()
+        response: HttpResponse = user_client.post(reverse('registration'), data=registration_data)  # type: ignore
+
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == reverse('games:home')
+        assert old_count_users == User.objects.count()
+
+    @pytest.mark.parametrize(
+            'vistor, expected_status',
+            [
+                (lf('owner_client'), HTTPStatus.OK),
+                (lf('user_client'), HTTPStatus.FORBIDDEN),
+                (lf('client'), HTTPStatus.FORBIDDEN),
+            ]
+    )
+    def test_only_authenticated_user_can_edit_profile(self, vistor: Client, expected_status: int, owner: User):
+        """Только авторизованный пользователь может менять свои данные."""
+        update_user_data: dict[str, str] = {
+            'username': 'owner_game',
+            'email': 'owner_game@django.ru',
+            'first_name': 'owner',
+            'last_name': 'game',
+        }
+        response: HttpResponse = vistor.put(  # type: ignore
+            reverse('users:update', args=(owner.username,)),
+            data=update_user_data,
+        )
+
+        assert response.status_code == expected_status
