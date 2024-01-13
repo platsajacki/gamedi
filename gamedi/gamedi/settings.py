72ba2e5962728e00123e19a483d7692961ef11ce
Вячеслав Менюхов <menyukhov@bk.ru>
2024-01-13 12:07:35 +0300

    Transfer templates to static.

--- gamedi/gamedi/settings.py
+++ gamedi/gamedi/settings.py
@@ -16,6 +16,12 @@ DEBUG = getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', 'localhost, 127.0.0.1').split(', ')
 
+CSRF_TRUSTED_ORIGINS = getenv(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://127.0.0.1, https://localhost, '
+    'https://www.127.0.0.1, https://www.localhost'
+).split(', ')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,9 +52,7 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+INTERNAL_IPS = ['127.0.0.1']
 
 ROOT_URLCONF = 'gamedi.urls'
 
@@ -58,9 +62,35 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'games:home'
 
-TEMPLATES_DIR = BASE_DIR / 'templates'
-
 CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
+
+WSGI_APPLICATION = 'gamedi.wsgi.application'
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('POSTGRES_DB'),
+        'USER': getenv('POSTGRES_USER'),
+        'PASSWORD': getenv('POSTGRES_PASSWORD'),
+        'HOST': getenv('DB_HOST'),
+        'PORT': getenv('DB_PORT'),
+    }
+}
+
+
+# Static files (HTML, CSS, JavaScript, Images)
+STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+TEMPLATES_DIR = STATIC_DIR / 'templates'
 
 TEMPLATES = [
     {
@@ -77,21 +107,6 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = 'gamedi.wsgi.application'
-
-
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv('DB_NAME'),
-        'USER': getenv('DB_USER'),
-        'PASSWORD': getenv('DB_PASSWORD'),
-        'HOST': getenv('DB_HOST'),
-        'PORT': getenv('DB_PORT'),
-    }
-}
 
 
 # Password validation
@@ -121,20 +136,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATIC_DIR = BASE_DIR / 'static'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
