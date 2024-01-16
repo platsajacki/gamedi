5a7947885dedcdda61b229bfbbd01bf4c6e82b05
Вячеслав Менюхов <menyukhov@bk.ru>
2024-01-16 16:53:21 +0300

    Delete IMAGEFILE.

--- gamedi/conftest.py
+++ gamedi/conftest.py
@@ -1,18 +1,11 @@
 import pytest
+
 from tempfile import NamedTemporaryFile
 
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client
 
 from games.models import Game, Genre
 from users.models import User
-
-IMAGEFILE: SimpleUploadedFile = SimpleUploadedFile(
-    name='test_image.jpg',
-    content=open(f'{settings.BASE_DIR}/pytest_tests/image.png', 'rb').read(),
-    content_type='image/png',
-)
 
 
 @pytest.fixture
