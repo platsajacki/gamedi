f60e6ef862b5bcf1abfd4d8466324fb16467883f
Вячеслав Менюхов <menyukhov@bk.ru>
2024-01-18 22:21:37 +0300

    Fix bug fomset tests.

--- gamedi/pytest_tests/test_forms.py
+++ gamedi/pytest_tests/test_forms.py
@@ -5,20 +5,20 @@ from django.core.exceptions import ValidationError
 from games.forms import RoleMessageFormSet
 
 
-def test_role_message_form_set_valid_data(form_data: dict):
+def test_role_message_form_set_valid_data(formset_data: dict):
     """RoleMessageFormSet валидна при верных данных"""
-    assert RoleMessageFormSet(form_data).is_valid()
+    assert RoleMessageFormSet(formset_data).is_valid()
 
 
-def test_role_message_form_set_duplication_email(form_data: dict):
+def test_role_message_form_set_duplication_email(formset_data: dict):
     """Дублирование email запрещено."""
-    form_data['form-1-email'] = form_data['form-0-email']
+    formset_data['form-1-email'] = formset_data['form-0-email']
     with pytest.raises(ValidationError):
-        RoleMessageFormSet(form_data).clean()
+        RoleMessageFormSet(formset_data).clean()
 
 
-def test_role_message_form_set_empty_email(form_data: dict):
+def test_role_message_form_set_empty_email(formset_data: dict):
     """Все email должны быть заполнены."""
-    form_data['form-1-email'] = ''
+    formset_data['form-1-email'] = ''
     with pytest.raises(ValidationError):
-        RoleMessageFormSet(form_data).clean()
+        RoleMessageFormSet(formset_data).clean()
