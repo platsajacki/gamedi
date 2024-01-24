f47370173b2f90fd1d4bbb789b0faf591677c873
Вячеслав Менюхов <menyukhov@bk.ru>
2024-01-24 21:42:14 +0300

    Add username test.

--- gamedi/pytest_tests/test_users.py
+++ gamedi/pytest_tests/test_users.py
@@ -35,10 +35,17 @@ class TestUserRegistrationLogic:
         """Анонимный пользователь может успешно зарегистрироваться."""
         old_count_users: int = User.objects.count()
         response: HttpResponseRedirect = client.post(reverse('registration'), data=registration_data)  # type: ignore
-
         assert response.status_code == HTTPStatus.FOUND
         assert response.url == reverse('login')
         assert old_count_users < User.objects.count()
+
+    def test_anonymous_cannot_register_non_latin_username(self, client: Client, registration_data: dict[str, str]):
+        """Нельзя использовать не латинские буквы в юзернейме."""
+        registration_data['username'] = 'Привет'
+        old_count_users: int = User.objects.count()
+        response: HttpResponse = client.post(reverse('registration'), data=registration_data)  # type: ignore
+        assert response.status_code == HTTPStatus.OK
+        assert old_count_users == User.objects.count()
 
     def test_authorized_user_cannot_registation(self, user_client: Client, registration_data: dict[str, str]):
         """Автороризованный пользователь не может успешно зарегистрироваться."""
@@ -47,7 +54,6 @@ class TestUserRegistrationLogic:
             reverse('registration'),
             data=registration_data,
         )
-
         assert response.status_code == HTTPStatus.FOUND
         assert response.url == reverse('games:home')
         assert old_count_users == User.objects.count()
@@ -71,7 +77,6 @@ class TestUserRegistrationLogic:
             reverse('users:update', args=(owner.username,)),
             data=update_user_data,
         )
-
         assert response.status_code == expected_status
 
 
